9ea0eb4da33d5250169894a15e23e5383243fab4
Emmett Childs <emmettchilds@thegobospot.com>
2022-01-07 08:21:43 +0800

    syntax

--- pages/utils/load-contract.js
+++ pages/utils/load-contract.js
@@ -1,6 +1,6 @@
 import contract from "@truffle/contract";
 
-export const loadContract = async (name, provider) => {
+const loadContract = async (name, provider) => {
     const res = await fetch(`/contracts/${name}.json`);
     const Artifact = await res.json();
     const _contract = contract(Artifact);
@@ -14,3 +14,5 @@ export const loadContract = async (name, provider) => {
     }
     return deployedContract;
 };
+
+export default loadContract;
